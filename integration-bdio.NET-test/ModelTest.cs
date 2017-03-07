e1702f027127cae8b43d7896d8244c90f0b06605
Jake Mathews <mathewsj2@wit.edu>
2017-03-06 19:06:00 -0500

    Fixed Tests

--- integration-bdio.NET-test/ModelTest.cs
+++ integration-bdio.NET-test/ModelTest.cs
@@ -12,14 +12,15 @@ namespace Com.Blackducksoftware.Integration.Hub.Bdio.Simple
         {
             string sampleBdio = Resources.sample;
             BdioContent content = BdioContent.Parse(sampleBdio);
-            BdioContent content2 = content;
+            BdioContent content2 = BdioContent.Parse(sampleBdio);
             Assert.AreEqual(6, content.Count);
             Assert.IsNotNull(content.ToString());
             Assert.IsTrue(content.Equals(content2));
             Assert.IsFalse(content.Equals(null));
-            Assert.AreNotEqual(0,content.GetHashCode());
+            Assert.AreNotEqual(0, content.GetHashCode());
 
             BdioComponent component = new BdioComponent();
+            component.Id = "Not Good";
             content2.Components.Add(component);
             Assert.IsFalse(content.Equals(content2));
         }
